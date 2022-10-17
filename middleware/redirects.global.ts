383230fe163ff1fbff7a8299769f7486df62848a
Evan <e.sende-ext@paris-turf.com>
2022-10-18 01:09:44 +0200

    Remove useless files

--- middleware/redirects.global.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(({ path }) => {
-  const user = useSupabaseUser()
-  if (!user.value && path !== '/login' && path !== '/sign-in') {
-    return navigateTo({ path: '/login' });
-  }
-});
